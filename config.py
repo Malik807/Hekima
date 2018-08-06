a22ab27c70dd9512f9966521eb28b91d3507967a
Malik807 <Malik807@users.noreply.github.com>
2018-08-06 08:58:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Initialization Account Info
-key = 'fbd2ce4992f3bd1801735709438efb56'
-b64secret = 'sUWA2+A8/LomqTFDexospJJE/CHZ8ihlIU+cRCpAzryvp+vLyPkNaY13ghQe11/wkpvJuOG+2ZeWAE67QWYGDw=='
-passphrase = 'KingsCraft4'
+key = 'DFasfasdasdasdsasdasd'
+b64secret = 'asdasdassdsadasdaddsdadada'
+passphrase = 'adasdadasdasdasdsdasdadas'
